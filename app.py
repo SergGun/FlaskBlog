ac46928aa8722f35d39dd96aae3176dc114201bf
Serg <wpserggun@gmail.com>
2018-02-06 22:57:20 +0200

    f

--- app.py
+++ app.py
@@ -1,38 +1,49 @@
 from flask import Flask, render_template, request, redirect, url_for
-from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 db = SQLAlchemy(app)
 
+
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     subtitle = db.Column(db.String(50))
-    author = db.Column(db.String(50))
+    author = db.Column(db.String(20))
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+    return render_template('index.html', posts=posts)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+
+    return render_template('post.html', post=post)
+
 
 @app.route('/add')
 def add():
     return render_template('add.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 @app.route('/addpost', methods=['POST'])
 def addpost():
@@ -40,12 +51,11 @@ def addpost():
     subtitle = request.form['subtitle']
     author = request.form['author']
     content = request.form['content']
-
-    post = Blogpost(title = title, subtitle = subtitle, author = author, content = content, date_posted = datetime.now())
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
     db.session.add(post)
     db.session.commit()
-
     return redirect(url_for('index'))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
